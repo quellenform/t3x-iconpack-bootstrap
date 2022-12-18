2d182272eeb81ee29b671f8d5ff70329ebe372ba
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2022-12-18 22:37:47 +0100

    Fixed description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Iconpack: Bootstrap Icons',
-    'description' => 'This Extension registers an Iconpack-Provider for `EXT:iconpack` which allows you to use the "Bootstrap Icons" in TYPO3.',
+    'description' => 'This Extension registers an Iconpack-Provider for EXT:iconpack which allows you to use the "Bootstrap Icons" in TYPO3.',
     'category' => 'fe',
     'state' => 'stable',
     'clearcacheonload' => true,
